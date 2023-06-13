15403fbf35c0376e22a21af4ef5b43e8aec097a6
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-14 01:07:05 +0700

    respoive HeaderComponent

--- src/component/HeaderComponent.jsx
+++ src/component/HeaderComponent.jsx
@@ -26,25 +26,24 @@ const HeaderComponent = () => {
             <img src={intro_desktop} alt="intro_desktop" className=" absolute top-[6.5rem] right-[10rem] w-[40%]"/>
             <img src={intro_left_desktop} alt="intro_left_desktop" className=" absolute bottom-0  left-0"/>
         </section>
-        <section>
-            <img src={intro_mobile} alt="intro_mobile" className=" w-full xl:hidden"/>
+        <section className="pb-[10rem] relative xl:hidden">
+            <img src={intro_mobile} alt="intro_mobile" className=" w-full"/>
+            <div className=" bg-[#2d2640] text-center px-[1.5rem] py-[6rem] relative overflow-hidden">
+                <img src={intro_left_mobile} alt="intro_left_mobile" className=" absolute z-[1] left-0 top-0"/>
+                <div className=" z-[10]">
+                    <h1 className=" font-fontDM text-white text-[2.8rem] leading-[2.8rem] ">Humanizing your insurance.</h1>
+                    <p className=" text-[#95a9c6] pt-[1rem] pb-[2rem] px-[0.5rem]">Get your life insurance coverage easier and faster. We blend our expertise 
+                        and technology to help you find the plan that’s right for you. Ensure you 
+                        and your loved ones are protected.
+                    </p>
+                    <button className=" font-fontKarla text-[#fafafa] px-[2rem] py-[0.5rem] border-2 border-white">View plans</button>
+                </div>                
+            </div>
+            <img src={intro_right_mobile} alt="intro_right_mobile" className=" absolute z-[1]  bottom-0 right-0"/>
         </section>
 
 
-        {/* <img src={intro_right_desktop} alt="intro_right_desktop" className=" absolute z-[2] -top-[8%] right-0"/>
-        <div className="flex justify-between lg:relative">
-            <div className="w-[50%] lg:pt-[3rem] ">
-                <h1 className=" font-fontDM text-[#fafafa] text-[4rem] lg:leading-[4rem] lg:pr-[7rem]">
-                    <span className=" lg:border-t lg:border-white lg:pt-[2.4rem] ">Hum</span>anizing your insurance.
-                </h1>
-                <p className=" text-[#95a9c6] lg:pt-[1rem] lg:pb-[2rem] lg:pr-[4rem]">Get your life insurance coverage easier and faster. We blend our expertise 
-                and technology to help you find the plan that’s right for you. Ensure you 
-                and your loved ones are protected.
-                </p>
-                <button className=" font-fontKarla text-[#fafafa] px-[2rem] py-[0.5rem] border-2 border-white">View plans</button>
-            </div>            
-        </div>
-        <img src={intro_desktop} alt="" className="w-[45%]  absolute top-0 right-0" /> */}
+      
     </header>
   )
 }
