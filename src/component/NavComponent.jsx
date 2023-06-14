286fabbf18e1f67e43112a717f31b04b156da347
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-14 16:18:58 +0700

    useContext for menu mobile

--- src/component/NavComponent.jsx
+++ src/component/NavComponent.jsx
@@ -1,21 +1,29 @@
 import logo from "../images/logo.svg";
 import ihambergur from "../images/icon-hamburger.svg";
+import iclose from "../images/icon-close.svg";
+import { useContext } from "react";
+import DataContext from "../DataContext";
 
 
 const NavComponent = () => {
+  const  {clickmenu,setClickmenu}= useContext(DataContext)
+
+  const OnClickmenu=()=>{
+        const current = !clickmenu;
+        setClickmenu(current)
+  }
+
   return (
-    <nav className="flex justify-between items-center p-[1.5rem] lg:px-[8rem] lg:py-[2rem]">
-        <div>
-            <img src={logo} alt="logo" />
-        </div>
-        <ul className="hidden lg:flex ">
+    <nav className="flex justify-between items-center p-[1.5rem] xl:px-[8rem] xl:py-[2rem]">
+        <a href="#"><img src={logo} alt="logo" /></a>
+        <ul className="hidden xl:flex ">
             <li className="nav-link-header"><a href="#">How we work</a></li>
             <li className="nav-link-header"><a href="#">Blog</a></li>
             <li className="nav-link-header"><a href="#">Account</a></li>
-            <li className="nav-link-header"><a href="#">View plans</a></li>
+            <li className="nav-link-header nav-link-select-header"><a href="#">View plans</a></li>
         </ul>
-        <div className=" lg:hidden">
-            <img src={ihambergur} alt="ihambergur" />
+        <div className=" xl:hidden">
+            <img src={clickmenu ?iclose :ihambergur} alt="ihambergur" className=" hover:cursor-pointer" onClick={OnClickmenu}/>
         </div>
     </nav>
   )
