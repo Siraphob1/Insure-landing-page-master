822a492454585408ed7cee273f26052e0b85f597
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-14 16:18:09 +0700

    edit class

--- src/component/Section2Component.jsx
+++ src/component/Section2Component.jsx
@@ -5,13 +5,13 @@ import how_mobile from "../images/bg-pattern-how-we-work-mobile.svg";
 
 const Section2Component = () => {
   return (
-    <section    className=" bg-[#2d2640] flex flex-col items-center text-white  relative
-                            xl:flex-row xl:justify-between  my-[4rem] py-[4rem] px-[1rem] xl:my-[8rem] xl:px-[5rem]">
+    <section    className=" bg-[#2d2640] flex flex-col items-center text-white  relative my-[8rem] py-[4rem] px-[1rem]
+                            xl:flex-row xl:justify-between  xl:my-[8rem] xl:px-[5rem]">
         <img src={how_desktop} alt="how_desktop" className="hidden z[1] absolute top-0 right-0 xl:block "/>
         <img src={how_mobile} alt="how_mobile" className=" z[1] absolute top-0 right-0 xl:hidden"/>
         
         <h1 className="z-[2] font-fontDM text-[2.5rem] leading-[2.5rem] text-center mb-[3rem]
-                        xl:text-[3.6rem]  xl:leading-[4rem] xl:text-start xl:w-[50%] xl:mb-0">Find out more about how we work</h1>
+                        xl:text-[3.6rem]  xl:leading-[4rem] xl:text-start xl:w-[60%] xl:pr-[4rem] xl:mb-0">Find out more about how we work</h1>
         <button className=" z-[2] btn">How we work</button>
     </section>
   )
