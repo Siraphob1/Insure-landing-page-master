67cb043ffaa55becf02f8ff3f8a465e0e822f577
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-14 16:14:28 +0700

    import DataContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import DataContext from "./DataContext"
 import HeaderComponent from "./component/HeaderComponent"
 import MainComponent from "./component/MainComponent"
 import NavComponent from "./component/NavComponent"
@@ -6,13 +8,17 @@ import NavComponent from "./component/NavComponent"
 
 
 function App() {
+  const [clickmenu,setClickmenu] = useState(false)
+  
 
   return (
-    <>
+    <DataContext.Provider value={
+      {clickmenu,setClickmenu}
+    }>
       <NavComponent/>
       <HeaderComponent/>
       <MainComponent/>
-    </>
+    </DataContext.Provider>
   )
 }
 
