877cb2ea1ced2cca2823c7eca8cf218030d83aae
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-14 13:49:31 +0700

    add Section2Component

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -1,5 +1,5 @@
 import Section1Component from "./Section1Component"
-
+import Section2Component from "./Section2Component"
 
 
 
@@ -7,6 +7,7 @@ const MainComponent = () => {
   return (
     <main className="px-[1.5rem] lg:px-[8rem] xl:py-[2rem]">
         <Section1Component/>
+        <Section2Component/>
     </main>
   )
 }
