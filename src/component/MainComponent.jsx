b041a08a5205e5d919676dd234224e3f002e9da2
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-14 16:15:45 +0700

    add FooterComponent & edit class

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -1,3 +1,4 @@
+import FooterComponent from "./FooterComponent"
 import Section1Component from "./Section1Component"
 import Section2Component from "./Section2Component"
 
@@ -5,9 +6,12 @@ import Section2Component from "./Section2Component"
 
 const MainComponent = () => {
   return (
-    <main className="px-[1.5rem] lg:px-[8rem] xl:py-[2rem]">
-        <Section1Component/>
-        <Section2Component/>
+    <main className="">
+        <div className="px-[1.5rem] lg:px-[10rem] xl:py-[2rem]">
+          <Section1Component/>
+          <Section2Component/>
+        </div>
+        <FooterComponent/>
     </main>
   )
 }
